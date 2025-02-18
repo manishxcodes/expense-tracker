c67d6e74a7bae0b8371a30cd69c6be100572775c
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-02-18 22:50:25 +0530

    add card comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,32 @@
-import { Button } from "./components/ui/button"
+import { useEffect, useState } from "react"
+import { 
+  Card, 
+  CardHeader,
+  CardTitle,
+  CardDescription, 
+  CardContent
+} from "./components/ui/card"
 
 function App() {
+  const [totalSpent, setTotalSpent] = useState(0);
+
+  useEffect(() => {
+    async function fetchTotal() {
+      const response = await fetch("api/expenses/total-spent");
+      const data = await response.json();
+      setTotalSpent(data.total)
+    }
+    fetchTotal();
+  }, [])
+
   return (
-    <div className="flex flex-col bg-background max-w-md mx-auto gap-y-5 mt-5">
-      <Button>up</Button>
-      <Button>down</Button>
-    </div>
+    <Card className="w-[350px] m-auto">
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>The total amount you've spent.</CardDescription>
+      </CardHeader>
+      <CardContent>{totalSpent}</CardContent>
+    </Card>
   )
 }
 
