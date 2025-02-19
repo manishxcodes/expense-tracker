bbc26b38a827fb68ef14967cacf0017e3e72f651
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-02-19 21:02:57 +0530

    add tanstack dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@backend": path.resolve(__dirname,"../backend")
     }
   },
   server: {
